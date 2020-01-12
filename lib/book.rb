aa841ef1622cccdfcff4eb154f6da0d5effbbab5
Ricky dildy <rdildy92@gmail.com>
2020-01-12 05:41:43 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,9 +10,9 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  def genre = (genre)
+  def genre=(genre)
     @genre = genre
-    Genres << genre
+    Genres=genre
   end 
 
 end
