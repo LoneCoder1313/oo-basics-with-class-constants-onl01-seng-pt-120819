4b25b9e17be8c9b24253d7936df178032b068052
Ricky dildy <rdildy92@gmail.com>
2020-01-12 02:59:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  
+  Brands = []
+  
   def initialize(brand)
     @brand = brand
+    if !(Brands.include?(@brand))
+      Brands << @brand
+    end 
   end
 
   def cobble
